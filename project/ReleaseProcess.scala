1c8871f4f113903aa1efab82f58ce5ba3bc092e4
Krisztian Lachata <krisztian.lachata@gmail.com>
2016-01-24 17:34:27 +0000

    add release plugin

--- project/ReleaseProcess.scala
+++ project/ReleaseProcess.scala
@@ -8,6 +8,10 @@ import sbtrelease._
 
 object ReleaseProcess {
 
+  val showNextVersion = settingKey[String]("the future version once releaseNextVersion has been applied to it")
+  val showReleaseVersion = settingKey[String]("the future version once releaseNextVersion has been applied to it")
+
+
   def setVersionOnly(selectVersion: Versions => String): ReleaseStep = { st: State =>
     val vs = st.get(ReleaseKeys.versions).getOrElse(sys.error("No versions are set! Was this release part executed before inquireVersions?"))
 
@@ -35,12 +39,19 @@ object ReleaseProcess {
         setReleaseVersion,
         runTest,
         tagRelease
-//        pushChanges
+        //        pushChanges
       ),
       releaseVersion <<= (releaseVersionBump) (bumper => {
         ver => sbtrelease.Version(ver)
           .map(_.withoutQualifier)
           .map(_.bump(bumper).string).getOrElse(versionFormatError)
-      })
+      }),
+      showReleaseVersion <<= (version, releaseVersion) ((v, f) => f(v)),
+      showNextVersion <<= (version, releaseNextVersion) ((v, f) => f(v)),
+      // strip the qualifier off the input version, eg. 1.2.1-SNAPSHOT -> 1.2.1
+      releaseVersion := { ver => sbtrelease.Version(ver).map(_.withoutQualifier.string).getOrElse(versionFormatError) },
+
+      // bump the minor version and append '-SNAPSHOT', eg. 1.2.1 -> 1.3.0-SNAPSHOT
+      releaseNextVersion := { ver => sbtrelease.Version(ver).map(_.bumpMinor.asSnapshot.string).getOrElse(versionFormatError) }
     )
 }
