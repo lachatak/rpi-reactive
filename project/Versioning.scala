734fa2a2c47048c0576f524dd896078f77b9da73
Krisztian Lachata <krisztian.lachata@gmail.com>
2016-01-24 16:31:39 +0000

    test

--- project/Versioning.scala
+++ project/Versioning.scala
@@ -2,9 +2,16 @@ import com.typesafe.sbt.SbtGit.git
 
 object Versioning {
 
-  lazy val settings = Seq(
-    git.baseVersion := "1.0.0",
-    git.useGitDescribe := true
-  )
+  val VersionRegex = "v([0-9]+.[0-9]+.[0-9]+)-?(.*)?".r
 
+  lazy val settings = Seq(
+    git.baseVersion := "1.0.0"
+    //    git.useGitDescribe := true
+    //    git.gitTagToVersionNumber := {
+    //      case VersionRegex(v, "SNAPSHOT") => Some(s"$v-SNAPSHOT")
+    //      case VersionRegex(v, "") => Some(v)
+    //      case VersionRegex(v, s) => Some(s"$v-$s-SNAPSHOT")
+    //      case _ => None
+    //    }
+  )
 }
